56a317d2983b37e5185e145b0966426943052642
Bruna Lima <brunalima2160@hotmail.com>
2022-11-23 11:59:18 -0400

    :style: estilização e estruturação da página de login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './pages/login';
+import '../src/style/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import Logo from "../../assets/logo-burger.svg";
 import Input from "../../components/inputs";
 import Button from "../../components/buttons";
+import "./style.css";
 
 function Login() {
   return (
     <>
       <section className="headerLogin">
-        <h1>BURGER QUEEN</h1>
-        <img src={Logo} alt="Logo da empresa" />
+        <h1 className="titleBQ">BURGER QUEEN</h1>
+        <img src={Logo} alt="Logo da empresa" className="logoLogin"/>
       </section>
 
       <section className="formLogin">
-        <p>Email:</p>
+        <h1 className="titleLogin">Login</h1>
+        <p className="label">Email:</p>
         <Input className='inputLogin' placeholder="Digite seu email" />
-        <p>Senha:</p>
-        <Input className='inputLogin' placeholder="Digite sua senha" />
+        <p className="label">Senha:</p>
+        <Input className='inputLogin' type='password' placeholder="Digite sua senha" />
         <p className="forgotPassword">Esqueceu sua senha?</p>
         <Button className="buttonLogin">Entrar</Button>     
       </section>
